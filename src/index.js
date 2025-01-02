043248a39a55a6af1b53e8bb93574d8e5be44e45
Dahvine <oluchukwudivine261@gmail.com>
2025-01-02 19:51:53 +0100

    Completed the menu bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "../src/style.css";
 import "../src/assets/fontawesome/css/all.min.css";
 
+const menuBtn = document.querySelector(".menu-btn");
+const navHolder = document.querySelector(".nav-holder");
+
+let menustatus = true;
+
+menuBtn.addEventListener("click", () => {
+  if (menustatus) {
+    navHolder.style.height = "180px";
+    menuBtn.classList.remove("fa-bars");
+    menuBtn.classList.add("fa-times");
+    menustatus = false;
+  } else {
+    navHolder.style.height = "70px";
+    menuBtn.classList.remove("fa-times");
+    menuBtn.classList.add("fa-bars");
+    menustatus = true;
+  }
+});
