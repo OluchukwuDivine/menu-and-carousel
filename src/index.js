c1025f881094c5c6a188a7c12c577ada06e16306
Dahvine <oluchukwudivine261@gmail.com>
2025-01-02 16:40:03 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "../src/style.css";
+import "../src/assets/fontawesome/css/all.min.css";
--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,48 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true, // Cleans the dist folder before each build
-    assetModuleFilename: 'assets/[hash][ext][query]' // Output path for assets
+    assetModuleFilename: "assets/[hash][ext][query]", // Output path for assets
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
     port: 8080,
-    hot: true
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+      // {
+      //   test: /\.svg$/,
+      //   use: [
+      //     {
+      //       loader: "svg-inline-loader",
+      //     },
+      //   ],
+      // },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
+      template: "./src/index.html",
+      filename: "index.html",
     }),
-  ]
-}
+  ],
+};
