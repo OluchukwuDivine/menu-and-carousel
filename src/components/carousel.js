32744ef005f0474430cd0e5a39fbb0d415a2a8ad
Dahvine <oluchukwudivine261@gmail.com>
2025-01-03 05:25:23 +0100

    updating the carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,12 +2,17 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const dots = document.querySelectorAll(".dots");
 const slides = document.querySelectorAll(".slides");
+const mainSlider = document.querySelector(".main-slider");
+
 
 currentSlide = 0;
 
 prevBtn.addEventListener("click", () => {
-    slides.classList.add("check");
+    mainSlider.classList.add("");
+    mainSlider.style.transform = "translate(-300px, 0)";
+});
+nextBtn.addEventListener("click", () => {
+  mainSlider.classList.add("slide-transition");
+  mainSlider.style.transform = "translate(300px, 0)";
 });
 
-// console.log(typeof(slides));
-// console.log(slides[0]);
