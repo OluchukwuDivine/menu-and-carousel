93c1656f82d3e5ba06afa199112cedddc979904a
Dahvine <oluchukwudivine261@gmail.com>
2025-01-05 09:31:50 +0100

    updated

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -3,16 +3,56 @@ const nextBtn = document.querySelector(".next-btn");
 const dots = document.querySelectorAll(".dots");
 const slides = document.querySelectorAll(".slides");
 const mainSlider = document.querySelector(".main-slider");
+const slide1 = "../src/assets/images/img1.jpeg";
+const slide2 = "../src/assets/images/img2.jpeg";
+const slide3 = "../src/assets/images/img3.jpeg";
 
-
-currentSlide = 0;
+const imgArray = [slide1, slide2, slide3];
+const randomIndex = Math.floor(Math.random() * imgArray.length);
+// const randomElement = myArray[randomIndex];
+console.log(randomIndex);
 
 prevBtn.addEventListener("click", () => {
-    mainSlider.classList.add("");
-    mainSlider.style.transform = "translate(-300px, 0)";
+    mainSlider.style.backgroundImage = `url(${imgArray[randomIndex]})`;
 });
 nextBtn.addEventListener("click", () => {
-  mainSlider.classList.add("slide-transition");
-  mainSlider.style.transform = "translate(300px, 0)";
+  mainSlider.style.backgroundImage = `url(${imgArray[2]})`;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// currentSlide = 0;
+
+// prevBtn.addEventListener("click", () => {
+//     mainSlider.classList.add("");
+//     mainSlider.style.transform = "translate(-300px, 0)";
+// });
+// nextBtn.addEventListener("click", () => {
+//   mainSlider.classList.add("slide-transition");
+//   mainSlider.style.transform = "translate(300px, 0)";
+// });
 
