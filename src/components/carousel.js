89714c07f268271e6f012b7ce22a0fb47276cafe
Dahvine <oluchukwudivine261@gmail.com>
2025-01-06 06:37:20 +0100

    updated

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -2,7 +2,6 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const dots = document.querySelectorAll(".img-dots");
 
-
 prevBtn.addEventListener("click", () => {
     alert("This section has not been updated");
 });
