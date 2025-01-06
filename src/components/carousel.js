9f47fe1ad1d10e0eb0d795fec960bd17623ed5af
Dahvine <oluchukwudivine261@gmail.com>
2025-01-06 06:35:53 +0100

    updated

---
+++ src/components/carousel.js
@@ -0,0 +1,17 @@
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const dots = document.querySelectorAll(".img-dots");
+
+
+prevBtn.addEventListener("click", () => {
+    alert("This section has not been updated");
+});
+nextBtn.addEventListener("click", () => {
+    alert("This section has not been updated");
+});
+
+dots.forEach(dot => {
+    dot.addEventListener("click", () => {
+      alert("This section has not been updated");
+    });
+});
